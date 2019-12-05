21c7758b0390e277ca613218b93eeba1c98b3b17
ki_blank <lbin7310@gmail.com>
2019-12-05 15:42:47 +0900

    Modify loginSecret of confirmSecret

--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -7,7 +7,12 @@ export default {
       const { email, secret } = args;
       const user = await prisma.user({ email });
       if (user.loginSecret === secret) {
-        // JWT
+        await prisma.updateUser({
+          where: { id: user.id },
+          data: {
+            loginSecret: ""
+          }
+        });
         const token = generateToken(user.id);
         return token;
       }
