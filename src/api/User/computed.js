68772f87a2ef7c34f06bf137355a686e0765a24d
ki_blank <lbin7310@gmail.com>
2019-12-06 14:53:18 +0900

    Add isFollowing, isSelf function

---
+++ src/api/User/computed.js
@@ -0,0 +1,26 @@
+import { prisma } from "../../../generated/prisma-client";
+
+export default {
+  User: {
+    fullName: parent => {
+      return `${parent.firstName} ${parent.lastName}`;
+    },
+    isFollowing: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      try {
+        const exists = await prisma.$exists.user({
+          AND: [{ id: user.id }, { followings_some: { id: parentId } }]
+        });
+        return exists;
+      } catch (error) {
+        return false;
+      }
+    },
+    isSelf: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return user.id === parentId;
+    }
+  }
+};
